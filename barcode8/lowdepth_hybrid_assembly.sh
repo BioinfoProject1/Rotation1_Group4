fd77c278a19d271f5291229a4c36945c3bdc3256
ZiqiXiong919 <alyzx23@nottingham.ac.uk>
2025-04-20 11:34:47 +0100

    Create lowdepth_hybrid_assembly.sh

---
+++ barcode8/lowdepth_hybrid_assembly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=100G
+#SBATCH --time=23:00:00
+#SBATCH --job-name=lowdepth_hybrid
+#SBATCH --output=/share/BioinfMSc/rotation1/Group4/barcode8/genome_assembly/lowdepth_hybrid/%x-%j.out
+#SBATCH --error=/share/BioinfMSc/rotation1/Group4/barcode8/genome_assembly/lowdepth_hybrid/%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbyle1@nottingham.ac.uk
+
+# Load environment
+source $HOME/.bash_profile
+
+# Activate unicycler environment
+conda activate unicycler2
+
+# Define input and NEW output paths
+R1="/share/BioinfMSc/rotation1/Group4/barcode8/short_read/R1merged_08.fastq"
+R2="/share/BioinfMSc/rotation1/Group4/barcode8/short_read/R2merged_08.fastq"
