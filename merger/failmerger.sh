f7857a4d30c141ae2e95e965a2191a11cd1e439b
ZiqiXiong919 <alyzx23@nottingham.ac.uk>
2025-04-20 18:14:31 +0100

    Create failmerger.sh

---
+++ merger/failmerger.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=5g
+#SBATCH --time=00:50:00
+#SBATCH --job-name=failmerger
+#SBATCH --output=/share/BioinfMSc/rotation1/Group4/long_read/nanoplot/failnanoplot2/%x-%j.out
+#SBATCH --error=/share/BioinfMSc/rotation1/Group4/long_read/nanoplot/failnanoplot2/%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mbyle1@nottingham.ac.uk
+
+# source your bash profile so you can use conda
+source $HOME/.bash_profile
+
+# create a conda environment
+conda activate nanoplot
+
+cat /share/BioinfMSc/Matt_resources/long_reads/fastq_fail/barcode04/*.fastq.gz > /share/BioinfMSc/rotation1/Group4/long_read/nanoplot/failnanoplot2/failed_merged_barcode04.fastq.gz 
+
+#nanoplot
+NanoPlot --fastq /share/BioinfMSc/rotation1/Group4/long_read/nanoplot/failnanoplot2/failed_merged_barcode04.fastq.gz -o /share/BioinfMSc/rotation1/Group4/long_read/nanoplot/failnanoplot2
+
+conda deactivate
